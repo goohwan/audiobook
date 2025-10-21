d83d6efb7d96b5bdc6296c06a029859cd2bf87dc
goohwan <goohwan@gmail.com>
2025-10-21 20:10:21 +0900

    1

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const $ = (selector) => document.querySelector(selector);
 let $fileInput, $fullScreenDropArea, $fileList, $textViewer, $voiceSelect, $rateSlider, $rateDisplay, $playPauseBtn;
 let $sequentialReadCheckbox, $clearAllFilesBtn;
 
-const INITIAL_TEXT_VIEWER_TEXT = '텍스트, 이미지 파일을 드래그하여 첨부하거나 텍스트/URL을 붙여넣어 오디오북으로 변환하세요!/n 모바일에선 파일첨부, 음성로드 버튼을 활용해주세요';
+const INITIAL_TEXT_VIEWER_TEXT = '텍스트, 이미지 파일을 드래그하여 첨부하거나 텍스트/URL을 붙여넣어 오디오북으로 변환하세요!\n 모바일에선 파일첨부, 음성로드 버튼을 활용해주세요';
 const INITIAL_TEXT_VIEWER_CONTENT = `<p>${INITIAL_TEXT_VIEWER_TEXT}</p>`;
 
 // --- 초기화 ---
