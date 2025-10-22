c7052b723ad3c04308d81ddb31bde6b6f04e01a4
goohwan <goohwan@gmail.com>
2025-10-22 15:50:45 +0900

    1

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const VISIBLE_CHUNKS = 10; // 가상화: 한 번에 렌더링할 청크 수
 const URL_PATTERN = /^(http|https):\/\/[^\s$.?#].[^\s]*$/i; // URL 인식 패턴
 
 // --- 파일 관련 상수 추가 ---
-const TEXT_EXTENSIONS = ['.txt'];
+const TEXT_EXTENSIONS = ['.txt', 'pdf'];
 const IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.tiff', '.tif'];
 const ALLOWED_EXTENSIONS = [...TEXT_EXTENSIONS, ...IMAGE_EXTENSIONS];
 
